6a1e410b8b5ecb7e1709cc6e42b040931f2134f9
Emil Broqvist <58086400+EmilBroqvist@users.noreply.github.com>
2025-04-09 13:18:10 +0200

    Emils fina filer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage.jsx";
+import AccountPage from "./pages/AccountPage.jsx";
+import FavoriteRecipePage from "./pages/FavoriteRecipePage.jsx";
 
 function Logout() {
   localStorage.clear();
@@ -19,11 +22,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<div>About</div>} />
           <Route path="/login" element={<LoginPage />}/>
           <Route path="/logout" element={<Logout />}/>
           <Route path="/register" element={<RegisterAndLogout />}/>
+          <Route path="/account" element={<AccountPage />}/>
+          <Route path="/favorites" element={<FavoriteRecipePage />}/>
           <Route path="*" element={<h1>404 Not Found</h1>}></Route>
         </Routes>
       </BrowserRouter>
