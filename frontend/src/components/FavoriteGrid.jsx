ac718f97871b2f430486ce9bb39a5efe0ce73410
Emil Johansson <143517083+Emilhalv@users.noreply.github.com>
2025-04-23 17:46:41 +0200

    Dynamic servings and save recipes

---
+++ frontend/src/components/FavoriteGrid.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import RecipeCard from "./RecipeCard.jsx";
+import api from "../api.js";
+
+
+function FavoriteGrid() {
+    const [favorites, setFavorites] = useState([]);
+
+    const getFavorites = async () => {
+        try {
+            const res = await api.get('/api/recipes/favorite/');
+            setFavorites(res.data);
+        } catch (error) {
+            console.error('Error fetching favorite recipes: ', error);
+        }
+    }
+
+    return (
+        <div className="favorite-grid">
+            {favorites.map((favorite) => (
+                <RecipeCard
+                    key={favorite.id}
+                    recipe={favorite}
+                    refreshRecipes={getFavorites}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default FavoriteGrid;
